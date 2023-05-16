cf104bb84724378d9fb00792d5a15b04a89adf65
Lis Fredriksson <lisfredrikson@gmail.com>
2023-05-16 19:24:50 +0200

    config

---
+++ api/config.js
@@ -0,0 +1,6 @@
+const config = {
+    // Kopiera och klistra in din connection string
+    CONNECTION_STRING: 'mongodb+srv://joelfredriksson1991:Johannes32@nodes.fvcdsvb.mongodb.net/filmDatabas'
+}
+
+module.exports = config;
